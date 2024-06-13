40923f0fe366954a6464fb5c7065662cd59944a1
carlos-siches-it <carl.siches.it@gmail.com>
2024-06-13 16:55:29 -0500

    Adding Great expectations configurations and Great Expectations Tech Article

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# great_expectations
+# Working with Azure Functions and Great Expectations
+
+## Introduction
+Ensuring data quality is crucial in the data-driven world for making sound decisions. Great Expectations is a powerful tool that enables the validation and documentation of our data. Integrating it with Azure Functions creates a scalable and automated solution for data validation. 
+
+## Steps
+### Initialize Great Expectations
+Within the root directory of the project, run the following command. This will create a new data context that we will modify throughout the article.
+
+`great_expectations init`
+
--- main.py
+++ main.py
@@ -2,4 +2,34 @@ import great_expectations as gx
 
 context = gx.get_context()
 
-print (context)
+# datasource = context.sources.add_or_update_pandas(name="my_pandas_dataframe")
+
+# dataasset = datasource.add_csv_asset(name="my_data_asset",
+#                                      filepath_or_buffer="data\yellow_tripdata_sample_2019-01.csv"
+#                                      )
+
+
+# batch_request = dataasset.build_batch_request()
+
+
+# print (datasource)
+
+data_asset = context.get_datasource("my_pandas_dataframe").get_asset("my_data_asset")
+
+batch_request = data_asset.build_batch_request()
+
+context.add_or_update_expectation_suite("my_expectation_suite")
+# context.add_or_update_checkpoint("my_checkpoint")
+
+validator = context.get_validator(
+    batch_request=batch_request,
+    expectation_suite_name="my_expectation_suite"
+)
+
+result = validator.expect_column_values_to_not_be_null(column="vendor_id")
+
+validator.save_expectation_suite(discard_failed_expectations=False)
+
+print (result)
+
+
